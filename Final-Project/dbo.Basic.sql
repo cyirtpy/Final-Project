f4b8e5a02c4aca0e561bc6a64f47c17fccdddf7b
Simon <simon93.hong@gmail.com>
2025-06-11 15:17:33 +0800

    加入專案檔案。

---
+++ Final-Project/dbo.Basic.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Basic] (
+    [書名]      NVARCHAR(MAX)        NOT NULL,
+    [英文書名]    NVARCHAR(MAX) NULL,
+    [開頭字母]    NCHAR (1) NULL,
+    [作者]      NVARCHAR(MAX) NULL,
+    [出版社]     NVARCHAR(MAX) NULL,
+    [出版日期(年)] INT NULL,
+    [出版日期(月)] INT NULL,
+    [出版日期(日)] INT NULL,
+    [ISBN]    NCHAR (13) NULL,
+    PRIMARY KEY CLUSTERED ([書名] ASC)
+);
+
