fabf3a0a0ec555f967c9f40ae765ba540e600c84
Simon <simon93.hong@gmail.com>
2025-06-12 11:39:51 +0800

    新增一些資料,讀取資料

--- Final-Project/Form1.Designer.cs
+++ Final-Project/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.btnRead = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(12, 12);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.RowHeadersWidth = 51;
+            this.dgvBooks.RowTemplate.Height = 27;
+            this.dgvBooks.Size = new System.Drawing.Size(428, 426);
+            this.dgvBooks.TabIndex = 0;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(459, 12);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(129, 67);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "讀取資料";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.dgvBooks);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Button btnRead;
     }
 }
 
--- Final-Project/Form1.cs
+++ Final-Project/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,60 @@ namespace Final_Project
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlDb = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cntStr = @"Data Source=(localDB)\MSSQLLocalDB;" +
+                @"AttachDBFilename=|DataDirectory|Database2.mdf;";
+
+            try
+            {
+                sqlDb = new SqlConnection(cntStr);
+                sqlDb.Open();
+                btnRead.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string sqlStr = "SELECT b.書名, 英文書名, 作者 FROM Basic b ";
+
+            SqlCommand sqlCmd = new SqlCommand(sqlStr, sqlDb);
+            SqlDataReader sqlDr = sqlCmd.ExecuteReader();
+            int rowIndex = 0;
+
+            dgvBooks.Font = new Font("微軟正黑體", 10);
+
+            for (int i = 0; i < sqlDr.FieldCount; i++)
+                dgvBooks.Columns.Add("column" + (i + 1).ToString(), sqlDr.GetName(i));
+
+            while (sqlDr.Read() != false)
+            {
+                dgvBooks.Rows.Add();
+
+                for (int i = 0; i < sqlDr.FieldCount; i++)
+                    dgvBooks.Rows[rowIndex].Cells[i].Value =
+                        sqlDr.GetValue(i).ToString();
+                rowIndex++;
+            }
+
+            sqlDr.Close();
+            btnRead.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (sqlDb != null)
+                sqlDb.Close();  // 關閉資料庫連線
         }
     }
 }
